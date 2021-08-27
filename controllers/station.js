6b6d9d0270e53b480b927fe8f3b16ef27cb90528
Caroline Conway <76437312+Colarein@users.noreply.github.com>
2021-08-27 11:44:21 +0100

    Updated Station controller Logger Reads out Station IDs and Readings Run Successfully

--- controllers/station.js
+++ controllers/station.js
@@ -4,12 +4,14 @@ const logger = require('../utils/logger');
 const stationCollection = require('../models/station-store.js');
 
 const station = {
-  index(request, response) {
-    const viewData = {
-      title: 'Station',
-    };
-    response.render('station', viewData);
-  },
+    index(request, response) {
+      const stationId = request.params.id;
+      logger.info('Station id = ' + stationId);
+      const viewData = {
+        name: 'Station',
+      };
+      response.render('station', viewData);
+    },
 };
 
 module.exports = station;
